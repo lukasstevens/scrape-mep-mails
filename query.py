611bf1b2e90c5c1abe983139960d78e35638e761
Lukas Stevens <mail@lukas-stevens.de>
2020-04-23 20:14:47 +0200

    feat(query.py) add cli

--- query.py
+++ query.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
+import argparse
 import asyncio
 from bs4 import BeautifulSoup
 from pathlib import Path
 import re
 import requests
+import shutil
+import sys
 import sqlite3
 
-MEP_SITE_FOLDER = './mep_sites/'
-
-async def download_mep_sites():
+async def download_mep_sites(path):
     mep_list_req = requests.get('https://www.europarl.europa.eu/meps/en/full-list/all')
     assert mep_list_req.status_code is 200
 
@@ -24,13 +25,11 @@ async def download_mep_sites():
         mep_sites.append((s[2], mep_site))
 
 
-    Path(MEP_SITE_FOLDER).mkdir(parents=True, exist_ok=True)
-
     async def save_mep_site(mep_id, mep_site_url):
         mep_site_req = requests.get(mep_site_url)
         assert mep_site_req.status_code is 200
 
-        with open('./{}/{}.html'.format(MEP_SITE_FOLDER, mep_id), 'w') as mep_site_file:
+        with open('./{}/{}.html'.format(path, mep_id), 'w') as mep_site_file:
             mep_site_file.write(mep_site_req.text)
 
 
@@ -67,8 +66,7 @@ async def scrape(mep_site_path):
         return {'id': Path(mep_site_path).stem, 'name': name, 'eu_fraction': european_fraction, 'nation': nation,
                 'national_party': national_party, 'emails': emails, 'roles': statuses}
 
-async def scrape_all():
-    path = Path(MEP_SITE_FOLDER)
+async def scrape_all(path):
     statuses = set()
 
     files = list(path.glob('*.html'))
@@ -103,7 +101,7 @@ def init_db(conn):
 
     conn.commit()
 
-def save_to_db(meps, db='meps.db'):
+def save_to_db(meps, db):
     conn = sqlite3.connect(db)
     curs = conn.cursor()
 
@@ -129,10 +127,55 @@ def save_to_db(meps, db='meps.db'):
     conn.commit()
     conn.close()
 
+
+def download(args):
+    p = Path(args.output_dir)
+    if p.exists() and not args.force:
+        print('The path {} already exists. Consider using the --force option.'.format(str(p)))
+    else:
+        if p.exists():
+            shutil.rmtree(p)
+        p.mkdir()
+        asyncio.run(download_mep_sites(p))
+
+def initdb(args):
+    input_path = Path(args.input_dir)
+    output_path = Path(args.output)
+    if not input_path.is_dir():
+        print('The input directory {} does not exist.'.format(str(input_path)))
+        return
+    if output_path.exists() and not args.force:
+        print('The path {} already exists. Consider using the --force option.'.format(str(output_path)))
+    else:
+        if output_path.exists():
+            output_path.unlink()
+        loop = asyncio.get_event_loop()
+        meps = loop.run_until_complete(loop.create_task(scrape_all(input_path)))
+        save_to_db(meps, output_path)
+
+
 if __name__ == '__main__':
-    #asyncio.run(download_mep_sites())
-    loop = asyncio.get_event_loop()
-    res = loop.run_until_complete(loop.create_task(scrape_all()))
-    save_to_db(res)
-    #print(gen_mailto_link([mep for mep in res if mep['nation'] == 'Germany']))
+    parser = argparse.ArgumentParser()
+    subparsers = parser.add_subparsers(title='subcommands', dest='cmd')
+    subparsers.required = True
+
+    parser_download = subparsers.add_parser('download', aliases=['dl'], help='Download the websites of the MEPs.')
+    parser_download.set_defaults(func=download)
+    parser_download.add_argument('--output_dir', '-o', type=str, default='mep_sites/',
+            help='the output directory for the scraped MEP sites (default: %(default)s)')
+    parser_download.add_argument('--force', '-f', action='store_true',
+            help='a flag indicating whether the output-dir should be overwritten')
+
+
+    parser_initdb = subparsers.add_parser('initdb', help='Create and populate a SQLite3 database with the scraped data')
+    parser_initdb.set_defaults(func=initdb)
+    parser_initdb.add_argument('--input_dir', '-i', type=str, default='mep_sites/',
+            help='The directory of the scraped MEP websites (default: %(default)s)')
+    parser_initdb.add_argument('--output', '-o', type=str, default='meps.db',
+            help='Filename of the SQLite3 database (default: %(default)s)')
+    parser_initdb.add_argument('--force', '-f', action='store_true',
+            help='a flag indicating whether the database should be overwritten')
+
+    args = parser.parse_args(sys.argv[1:])
+    args.func(args)
 
